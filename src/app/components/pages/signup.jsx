7828685fa06b601821e6cf6eb769bee8b7ca630a
James Nocentini <james.nocentini@gmail.com>
2015-05-28 20:55:20 +0100

    add signup route

--- src/app/components/pages/signup.jsx
+++ src/app/components/pages/signup.jsx
@@ -8,19 +8,46 @@ var {
   } = mui;
 
 var SignUpPage = React.createClass({
+    handleSignup() {
+        var username = this.refs.username.getValue();
+        var password = this.refs.password.getValue();
+        var confirmPassword = this.refs.confirmPassword.getValue();
+        if (password == confirmPassword) {
+            fetch('/signup', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: username,
+                    password: password
+                })
+            })
+              .then(function(res) {
+                  console.log(res);
+              })
+              .catch(function(err) {
+                  console.log(err);
+              })
+        }
+    },
     render() {
         return (
           <div className="mui-app-content-canvas">
               <TextField
+                ref="username"
                 hintText="username" />
               <TextField
+                ref="password"
                 hintText="password" />
               <TextField
+                ref="confirmPassword"
                 hintText="confirm password" />
               <br/>
               <RaisedButton
                 label="sign up"
-                primary={true} />
+                primary={true}
+                onClick={this.handleSignup} />
           </div>
         );
     }
